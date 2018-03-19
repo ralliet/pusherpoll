7818b66e79f4459dd21e8b914e5bfb936db35806
Ruben Alliet <allietruben@gmail.com>
2018-03-19 16:05:16 +0100

    add event listener for form

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require("express");
-const path = requrie("path");
+const path = require("path");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+
+const poll = require("./routes/poll");
 
 //set Public folder
 app.use(express.static(path.join(__dirname,'public')));
 
 // Body parser middleware 
 app.use(bodyParser.json());
-app.use(bodyParse.urlencoded({extended: false }));
+app.use(bodyParser.urlencoded({extended: false }));
 
 //enable CORS
 app.use(cors());
+
+app.use('/poll',poll);
 
 const port = 3000;
 
---
+++ routes/poll.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+const Pusher = require('pusher');
+
+var pusher = new Pusher({
+    appId: '494374',
+    key: '33cff15253d6e12abb18',
+    secret: '2d9761e635c34d576033',
+    cluster: 'eu',
+    encrypted: true
+});
+
+router.get('/',(req,res) => {
+    res.send('POLL');
+});
+
+router.post('/',(req,res) => {
+    pusher.trigger('os-poll', 'os-vote', {
+        points: 1,
+        os: req.body.os
+    });
+
+    return res.json({success:true, message: 'Thank you for voting'});
+})
+
+
+module.exports = router;
