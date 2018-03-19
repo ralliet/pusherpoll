53d6bbe58da3d84a71a8975301ca3fb071b47af5
Ruben Alliet <allietruben@gmail.com>
2018-03-19 20:15:07 +0100

    count votes for each os and output

--- public/main.js
+++ public/main.js
@@ -21,79 +21,75 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 });
 
-
 fetch('http://localhost:3000/poll')
     .then(res => res.json())
     .then(data => {
-        const votes = data.votes;
+        const votes = data.Votes;
         const totalVotes = votes.length;
-        // Count vote points for each 
-        votes.reduce((prev,curr) => {
-            
-        })
-    
-    })
-    .catch(err => console.log(err));
+        // Count vote points for each OS
+        const voteCounts = votes.reduce(
+            (acc, vote) => (
+                (acc[vote.os] = (acc[vote.os] || 0) + parseInt(vote.points)), acc),{});
 
-let dataPoints = [
-    {
-        label: 'windows',
-        y: 0
-    }, {
-        label: 'macos',
-        y: 0
-    }, {
-        label: 'linux',
-        y: 4
-    }, {
-        label: 'other',
-        y: 0
-    }
-];
+        console.log(voteCounts);
 
-const chartContainer = document.querySelector('#chartContainer')
-
-//is there a chartcontainer?
-if (chartContainer) {
-    const chart = new CanvasJS.Chart("chartContainer", {
-        theme: "light1", // "light2", "dark1", "dark2"
-        animationEnabled: true, // change to true
-        title: {
-            text: "Basic Column Chart"
-        },
-        data: [
+        let dataPoints = [
             {
-                // Change type to "bar", "area", "spline", "pie",etc.
-                type: "column",
-                dataPoints: dataPoints
+                label: 'windows',
+                y: voteCounts['windows']
+            }, {
+                label: 'macos',
+                y: voteCounts['macos']
+            }, {
+                label: 'linux',
+                y: voteCounts['linux']
+            }, {
+                label: 'other',
+                y: voteCounts['other']
             }
-        ]
-    });
-    chart.render();
+        ];
 
+        const chartContainer = document.querySelector('#chartContainer')
 
-    // Enable pusher logging - don't include this in production
-    Pusher.logToConsole = true;
+        //is there a chartcontainer?
+        if (chartContainer) {
+            const chart = new CanvasJS.Chart("chartContainer", {
+                theme: "light1", // "light2", "dark1", "dark2"
+                animationEnabled: true, // change to true
+                title: {
+                    text: "Basic Column Chart"
+                },
+                data: [
+                    {
+                        // Change type to "bar", "area", "spline", "pie",etc.
+                        type: "column",
+                        dataPoints: dataPoints
+                    }
+                ]
+            });
+            chart.render();
 
-    var pusher = new Pusher('33cff15253d6e12abb18', {
-        cluster: 'eu',
-        encrypted: true
-    });
+            // Enable pusher logging - don't include this in production
+            Pusher.logToConsole = true;
 
-    var channel = pusher.subscribe('os-poll');
-    channel.bind('os-vote', function (data) {
-        dataPoints = dataPoints.map((x) => { 
-            if(x.label === data.os) {
-                x.y += data.points;
-                return x;
-            } else {
-                return x;
-            }
-        });
-        chart.render();
-    });
-   
+            var pusher = new Pusher('33cff15253d6e12abb18', {
+                cluster: 'eu',
+                encrypted: true
+            });
 
-}
+            var channel = pusher.subscribe('os-poll');
+            channel.bind('os-vote', function (data) {
+                dataPoints = dataPoints.map((x) => {
+                    if (x.label === data.os) {
+                        x.y += data.points;
+                        return x;
+                    } else {
+                        return x;
+                    }
+                });
+                chart.render();
+            });
 
-//canvas.js
+        }
+
+    })
