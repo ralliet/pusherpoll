3a1f7e5e72093843901d123a1822599db7dfe9ca
Ruben Alliet <allietruben@gmail.com>
2018-03-19 15:04:09 +0100

    index + app.js

--- app.js
+++ app.js
@@ -7,3 +7,15 @@ const app = express();
 
 //set Public folder
 app.use(express.static(path.join(__dirname,'public')));
+
+// Body parser middleware 
+app.use(bodyParser.json());
+app.use(bodyParse.urlencoded({extended: false }));
+
+//enable CORS
+app.use(cors());
+
+const port = 3000;
+
+//start server
+app.listen(port,() => console.log(`Server started on port ${port}`));
